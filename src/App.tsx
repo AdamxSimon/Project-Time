82b58e5637dfa823d5a5399ed3c3ee13d106d164
Adam Simon <adampersimonal@gmail.com>
2022-09-04 14:43:12 -0500

    Initialized app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// Styles
+
+import classes from "./App.module.css";
+
+const App = (): JSX.Element => {
+  return <div className={classes.app}></div>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+// React
+
+import ReactDOM from "react-dom/client";
+
+// Components
+
+import App from "./App";
+
+// Styles
+
+import "./index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(<App />);
