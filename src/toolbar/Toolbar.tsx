3a59d94154ee92f12278f146058c38e0c3b7d778
Adam Simon <asimon@evernote.com>
2022-11-10 16:24:09 -0600

    Added styling for save and import buttons

--- src/toolbar/Toolbar.tsx
+++ src/toolbar/Toolbar.tsx
@@ -7,6 +7,10 @@ import { useContext } from "react";
 import { CurrencyContext } from "../context/CurrencyContext";
 import { ProjectContext, ProjectStatus } from "../context/ProjectContext";
 
+// Components
+
+import Button from "../components/Button/Button";
+
 // Assets
 
 import projectsIcon from "../assets/projects.png";
@@ -26,13 +30,28 @@ const Toolbar = (): JSX.Element => {
 
   return (
     <div className={classes.toolbar}>
-      <div className={classes.infoContainer}>
-        <img src={projectsIcon} alt="Projects" height={16} width={16} />
-        <div>{`${activeProjects.length}/${maxProjects}`}</div>
+      <div className={classes.infoContainers}>
+        <div className={classes.infoContainer}>
+          <img src={projectsIcon} alt="Projects" height={16} width={16} />
+          <div>{`${activeProjects.length}/${maxProjects}`}</div>
+        </div>
+        <div className={classes.infoContainer}>
+          <img src={coinIcon} alt="Currency" height={16} width={16} />
+          <div>{currency}</div>
+        </div>
       </div>
-      <div className={classes.infoContainer}>
-        <img src={coinIcon} alt="Currency" height={16} width={16} />
-        <div>{currency}</div>
+
+      <div className={classes.settingsContainer}>
+        <Button
+          text={"Save"}
+          onClick={() => {}}
+          style={{ backgroundColor: "lightgray" }}
+        />
+        <Button
+          text={"Import"}
+          onClick={() => {}}
+          style={{ backgroundColor: "lightgray" }}
+        />
       </div>
     </div>
   );
